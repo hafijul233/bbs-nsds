bd9dc3aa4c703012776b64cf1185be50752a336b
Mohammad Hafijul Islam <hafijul233@gmail.com>
2022-07-03 23:10:24 +0600

    add translation to dashboard navbar

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -1,7 +1,10 @@
+@push('plugin-style')
+    <link rel="stylesheet" href="{{ asset('plugins/flag-icon-css/css/flag-icon.min.css') }}">
+@endpush
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Menu sidebar toggle button -->
     <a class="nav-link text-decoration-none text-dark" data-widget="pushmenu" href="#" role="button"><i
-            class="fas fa-bars"></i></a>
+                class="fas fa-bars"></i></a>
 
     <!-- Left navbar links -->
 {{--@include('layouts.partials.navbar.navbar-shortcut')--}}
@@ -14,13 +17,28 @@
                 <i class="fas fa-expand-arrows-alt"></i>
             </a>
         </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link" data-toggle="dropdown" href="#" aria-expanded="false">
+                <i class="flag-icon @if(session()->get('locale') == 'bd') flag-icon-bd @else flag-icon-us @endif"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right p-0" style="left: inherit; right: 0;">
+                <a href="{{ route('translate-locale') }}?language=en"
+                   class="dropdown-item @if(session()->get('locale') == 'en') active @endif">
+                    <i class="flag-icon flag-icon-us mr-2"></i> English
+                </a>
+                <a href="{{ route('translate-locale') }}?language=bd"
+                   class="dropdown-item @if(session()->get('locale') == 'bd') active @endif">
+                    <i class="flag-icon flag-icon-bd mr-2"></i> Bangla
+                </a>
+            </div>
+        </li>
         <!-- Navbar Search -->
-        {{--@include('layouts.partials.navbar.navbar-search')--}}
-        <!-- Messages Dropdown Menu -->
-        {{--@include('layouts.partials.navbar.navbar-message')--}}
-        <!-- Notifications Dropdown Menu -->
-        {{--@include('layouts.partials.navbar.navbar-notification')--}}
-        <!-- User Profile Dropdown menu -->
+    {{--@include('layouts.partials.navbar.navbar-search')--}}
+    <!-- Messages Dropdown Menu -->
+    {{--@include('layouts.partials.navbar.navbar-message')--}}
+    <!-- Notifications Dropdown Menu -->
+    {{--@include('layouts.partials.navbar.navbar-notification')--}}
+    <!-- User Profile Dropdown menu -->
         @include('layouts.partials.navbar.navbar-user')
         {{--<li class="nav-item">
             <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
@@ -32,7 +50,8 @@
 <!-- /.navbar -->
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
     <div class="modal-dialog  modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
