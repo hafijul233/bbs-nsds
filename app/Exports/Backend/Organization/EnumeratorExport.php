402bfcb9a2749b6ababbb9bee7823414cf447bb1
mah.shamim <mah.shamim@gmail.com>
2022-08-02 01:28:40 +0600

    remove Enabled column

--- app/Exports/Backend/Organization/EnumeratorExport.php
+++ app/Exports/Backend/Organization/EnumeratorExport.php
@@ -75,7 +75,7 @@ class EnumeratorExport extends FastExcelExport
         //}
         $this->formatRow = array_merge($this->formatRow, [
             trans('Created By', [], 'en') => $row->created_by_username ?? 'null',
-            'Enabled' => ucfirst(($row->enabled ?? '')),
+            //'Enabled' => ucfirst(($row->enabled ?? '')),
             'Created Date' => $row->created_at->format(config('backend.datetime'))
         ]);
 
